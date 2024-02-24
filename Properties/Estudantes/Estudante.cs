38c51efe1c635eb2ddce5a4d1b7ea443b87133f3
Lukhas Daniel <lukhasfurtado9@gmail.com>
2024-02-24 17:46:45 -0300

    Criada a classe Estudante.cs

---
+++ Properties/Estudantes/Estudante.cs
@@ -0,0 +1,18 @@
+﻿namespace ApiCrud;
+
+public class Estudante
+{
+    public Guid Id{get; init;}
+
+    public string Nome { get; private set;}
+
+    public bool Ativo {get; private set;}
+
+    public Estudante(string nome)
+    {
+        this.Nome = nome;
+        Id = Guid.NewGuid();
+        Ativo = true;
+    }
+
+}
