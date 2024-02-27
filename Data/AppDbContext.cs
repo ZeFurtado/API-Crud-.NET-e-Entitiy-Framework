c5bb3ddf17df5e963d8cf0a41a1958ee3394ff7e
Lukhas Daniel <lukhasfurtado9@gmail.com>
2024-02-27 19:10:35 -0300

    Criada a classe de Contexto de Banco de Dados

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiCrud;
+
+public class AppDbContext : DbContext
+{
+        DbSet<Estudante> Estudantes {get; set;}
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite();
+        base.OnConfiguring(optionsBuilder);
+    }
+}
